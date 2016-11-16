c95111849b3d6a2d98d57f32f8d1ec4f4650c369
Steven Wart <steve@wart.ca>
2016-11-15 21:46:58 -0800

    Initial commit

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,17 @@
+
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+        if (points == null) throw new NullPointerException();
+        
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+        return segments.size;
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+    }
+}
